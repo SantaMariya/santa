790f2089e662a312ec7898a7205a0a7be329c323
SantaMariya <152678473+SantaMariya@users.noreply.github.com>
2025-07-15 09:07:06 +0000

    Final commit

--- policy.tf
+++ policy.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "ex1" {
-  user = aws_iam_role.ecx1.name
+resource "aws_iam_user_policy_attachment" "ex1" {
+  user = aws_iam_user.ex1.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
--- user_attachment.tf
+++ user_attachment.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user_group_membership" "membership" {
   user = aws_iam_user.ex1.name
   groups = [
-    aws_iam_group.group1.name
+    aws_iam_group.ex1.name
   ]
 }
